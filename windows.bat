e7094045430b630929c15fc135010bffb75db4e2
acc23of <freemanowen77@gmail.com>
2025-11-12 09:52:13 +0000

    Fix windows.bat

--- windows.bat
+++ windows.bat
@@ -1,22 +1,22 @@
-
 @echo off
 REM ============================
-REM Running Teaching Tool Web Server on Windows with Docker - author: Ayesha Sana
+REM Run Teaching Tool Web Server on Windows with Docker
 REM ============================
 
 REM ---- CONFIG ----
 set IMAGE_NAME=com3524
+set DISPLAY=host.docker.internal:0.0
 
 REM ---- BUILD IMAGE ----
 echo Building Docker image...
 docker build -t %IMAGE_NAME% .
 
-REM ---- RUN CONTAINER WITH INTERACTIVE SHELL ----
-echo Running web app on http://127.0.0.1:5000 ...
+REM ---- RUN CONTAINER ----
+echo Running app on http://127.0.0.1:5000 ...
 docker run -it ^
-    -e DISPLAY=host.docker.internal:0.0 ^
+    -e DISPLAY=%DISPLAY% ^
     -p 5000:5000 ^
     %IMAGE_NAME% ^
-    bash
+    python3 run_tool.py
 
 pause
